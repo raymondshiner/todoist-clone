b2013ad3bae91d3fc62d0d2bd2c79c52fc8dbb98
Raymond Shiner <raymondshiner@gmail.com>
2023-09-03 16:55:08 -0700

    better initial todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,9 +3,9 @@ import { Todo } from "../db/schema";
 
 export function TodoItem({ content, completed, id }: Todo) {
     return (
-        <div class="flex flex-row items-center border border-[#585858] p-2 px-3 my-2 rounded bg-[#282828] gap-2">
+        <div class="w-80 max-w-sm flex flex-row items-center border border-[#585858] p-2 px-3 my-2 rounded bg-[#282828] gap-2 relative group/item">
             <span
-                class="cursor-pointer rounded-full border border-gray-200 w-4 h-4 flex items-center justify-center pt-0.5 group"
+                class="cursor-pointer rounded-full border border-gray-200 w-4 h-4 flex items-center justify-center pt-0.5 group/icon shrink-0"
                 role="checkbox"
                 hx-post={`/todos/toggle/${id}`}
                 hx-swap="outerHTML"
@@ -14,19 +14,17 @@ export function TodoItem({ content, completed, id }: Todo) {
                 <i
                     class={`fa-solid fa-check fa-2xs ${
                         completed ? "opacity-100 text-green-400" : "opacity-0"
-                    } group-hover:opacity-100 transition-opacity duration-300`}
+                    } group-hover/icon:opacity-100 transition-opacity duration-300`}
                 />
             </span>
 
             <p>{content}</p>
-            <button
-                class="text-red-500 ml-auto cursor-pointer"
+            <i
+                class="fa-solid fa-trash text-red-400 ml-auto cursor-pointer opacity-0 group-hover/item:opacity-100"
                 hx-delete={`/todos/${id}`}
                 hx-swap="outerHTML"
                 hx-target="closest div"
-            >
-                X
-            </button>
+            ></i>
         </div>
     );
 }
