e6926ce2b32ed21327ebaf044521b1398329ee2b
Raymond Shiner <raymondshiner@gmail.com>
2023-09-03 10:47:44 -0700

    simple styling updates

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,17 +3,18 @@ import { Todo } from "../db/schema";
 
 export function TodoItem({ content, completed, id }: Todo) {
     return (
-        <div class="flex flex-row space-x-3">
-            <p>{content}</p>
+        <div class="flex flex-row  border border-[#585858] p-2 px-3 my-2 rounded bg-[#282828] gap-2">
             <input
                 type="checkbox"
+                class="cursor-pointer"
                 checked={completed}
                 hx-post={`/todos/toggle/${id}`}
                 hx-swap="outerHTML"
                 hx-target="closest div"
             />
+            <p>{content}</p>
             <button
-                class="text-red-500"
+                class="text-red-500 ml-auto cursor-pointer"
                 hx-delete={`/todos/${id}`}
                 hx-swap="outerHTML"
                 hx-target="closest div"
